f2a0c22c04d060c44dfb6b2acb461c6e42812a4d
Juan Hernández <hernanjua@colpatria.com>
2018-08-12 21:26:21 -0500

    added mysql config

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000,
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    mysql = require('mysql'),
+    myConnection = require('express-myconnection'),
+    dbOptions = require('./api/database');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(myConnection(mysql, dbOptions, 'single'));
 
 var eventRoutes = require('./api/routes/eventRoutes');
 eventRoutes(app);
