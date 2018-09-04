2f4064c47744590618644a518efeaf13e6adf737
Juan Hernández <hernanjua@colpatria.com>
2018-09-03 22:23:04 -0500

    modified competition model

--- sql/tables.sql
+++ sql/tables.sql
@@ -12,12 +12,11 @@ CREATE TABLE `Users` (
 CREATE TABLE `Competitions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(500) NOT NULL,
-  `category` varchar(25) NOT NULL,
-  `place` varchar(500) NOT NULL,
-  `address` varchar(200) NOT NULL,
+  `banner` varchar(2000) NOT NULL,
+  `address` varchar(400) NOT NULL,
   `init` datetime NOT NULL,
   `end` datetime NOT NULL,
-  `stage` varchar(20) NOT NULL,
+  `prize` varchar(2000) NOT NULL,
   `user` varchar(255) NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
