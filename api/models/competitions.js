3067e3162501a9b09a7deb52f11c8b655b68855d
Cristian Sepulveda <sepulvec@colpatria.com>
2018-09-06 13:26:52 -0500

    delete on cascade

--- api/models/competitions.js
+++ api/models/competitions.js
@@ -1,107 +1,124 @@
 'use strict'
 
-exports.list = function(db, user, callback) {
-    db(function(err, connection) {
+exports.list = function (db, user, callback) {
+    db(function (err, connection) {
         if (err) throw "Error on db: " + err;
         connection.query('SELECT * FROM Competitions where user = ? order by created desc', [user], function (error, results, fields) {
-            if (error){
+            if (error) {
                 console.log('Error performing select Competitions query: ' + error);
-                callback({code: 500});
+                callback({ code: 500 });
             }
             else {
-                callback({list: results});
+                callback({ list: results });
             }
         });
     });
 }
 
-exports.create = function(db, competition, user, callback) {
-    db(function(err, connection) {
+exports.create = function (db, competition, user, callback) {
+    db(function (err, connection) {
         if (err) throw "Error on db: " + err;
         var d = new Date();
         var dateCreated = "" + d.getFullYear() + "-" + d.getMonth() + "-" + d.getDay() + " " + d.getHours() + "-" + d.getMinutes() + "-" + d.getSeconds();
-        connection.query('INSERT INTO Competitions SET ?', {...competition, user: user, created: dateCreated}, function (error, results, fields) {
-            if (error){
+        connection.query('INSERT INTO Competitions SET ?', { ...competition, user: user, created: dateCreated }, function (error, results, fields) {
+            if (error) {
                 console.log('Error performing insert Competitions query: ' + error);
                 callback(false);
             }
-            else{
+            else {
                 callback(results.insertId);
             }
         });
     });
 }
 
-exports.find = function(db, id, callback) {
-    db(function(err, connection) {
+exports.find = function (db, id, callback) {
+    db(function (err, connection) {
         if (err) throw "Error on db: " + err;
         connection.query('SELECT * FROM Competitions where id = ?', [id], function (error, results, fields) {
-            if (error){
+            if (error) {
                 console.log('Error performing select Competitions by id query: ' + error);
-                callback({code: 500});
+                callback({ code: 500 });
             }
             else {
                 if (results.length == 0)
-                    callback({code: 404});
+                    callback({ code: 404 });
                 else
-                    callback({competition: results[0]});
+                    callback({ competition: results[0] });
             }
         });
     });
 }
 
-exports.findByURL = function(db, address, callback) {
-    db(function(err, connection) {
+exports.findByURL = function (db, address, callback) {
+    db(function (err, connection) {
         if (err) throw "Error on db: " + err;
         connection.query('SELECT * FROM Competitions where address = ?', [address], function (error, results, fields) {
-            if (error){
+            if (error) {
                 console.log('Error performing select Competitions by address query: ' + error);
-                callback({code: 500});
+                callback({ code: 500 });
             }
             else {
                 if (results.length == 0)
-                    callback({code: 404});
+                    callback({ code: 404 });
                 else
-                    callback({competition: results[0]});
+                    callback({ competition: results[0] });
             }
         });
     });
 }
 
-exports.update = function(db, competition, id, callback) {
-    db(function(err, connection) {
+exports.update = function (db, competition, id, callback) {
+    db(function (err, connection) {
         if (err) throw "Error on db: " + err;
         delete competition.created;
         connection.query("update Competitions set ? where id = ?",
-                        [competition, id], function (error, results, fields) {
-            if (error){
-                console.log('Error performing update Competitions by id query: ' + error);
-                callback({code: 500});
-            }
-            else {
-                if (results.affectedRows == 0)
-                    callback({code: 404});
-                else
-                    callback({code: 200});
-            }
-        });
+            [competition, id], function (error, results, fields) {
+                if (error) {
+                    console.log('Error performing update Competitions by id query: ' + error);
+                    callback({ code: 500 });
+                }
+                else {
+                    if (results.affectedRows == 0)
+                        callback({ code: 404 });
+                    else
+                        callback({ code: 200 });
+                }
+            });
     });
 }
 
-exports.delete = function(db, id, callback) {
-    db(function(err, connection) {
+exports.delete = function (db, id, callback) {
+
+    db(function (err, connection) {
         if (err) throw "Error on db: " + err;
-        connection.query('delete from Competitions where id = ?', [id], function (error, results, fields) {
-            if (error){
+        connection.query('delete from videos where fk_id_competition = ?', [id], function (error, results, fields) {
+            if (error) {
                 console.log('Error performing delete Competitions by id query: ' + error);
-                callback({code: 500});
+                callback({ code: 500 });
             }
             else {
                 if (results.affectedRows == 0)
-                    callback({code: 404});
-                else
-                    callback({code: 200});
+                    callback({ code: 404 });
+                else {
+                    db(function (err, connection) {
+                        if (err) throw "Error on db: " + err;
+                        connection.query('delete from Competitions where id = ?', [id], function (error, results, fields) {
+                            if (error) {
+                                console.log('Error performing delete Competitions by id query: ' + error);
+                                callback({ code: 500 });
+                            }
+                            else {
+                                if (results.affectedRows == 0)
+                                    callback({ code: 404 });
+                                else
+                                    callback({ code: 200 });
+                            }
+                        });
+                    });
+                }
             }
         });
     });
+
 }
--- sql/tables.sql
+++ sql/tables.sql
@@ -1,16 +1,14 @@
-CREATE TABLE `Users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `password` varchar(500) NOT NULL,
   `firstName` varchar(255) NOT NULL,
-  `lastName` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `Users_Name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+  `lastName` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
-CREATE TABLE `Competitions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `competitions` (
+  `id` int(11) NOT NULL,
   `name` varchar(500) NOT NULL,
   `banner` varchar(2000) NOT NULL,
   `address` varchar(400) NOT NULL,
@@ -18,9 +16,9 @@ CREATE TABLE `Competitions` (
   `end` datetime NOT NULL,
   `prize` varchar(2000) NOT NULL,
   `user` varchar(255) NOT NULL,
-  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 
 CREATE TABLE `videos` (
   `id_video` int(11) NOT NULL,
@@ -34,3 +32,56 @@ CREATE TABLE `videos` (
   `state_video` varchar(50) NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for table `competitions`
+--
+ALTER TABLE `competitions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `Users_Name` (`name`);
+
+--
+-- Indexes for table `videos`
+--
+ALTER TABLE `videos`
+  ADD PRIMARY KEY (`id_video`),
+  ADD KEY `fk_id_competition` (`fk_id_competition`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `competitions`
+--
+ALTER TABLE `competitions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `videos`
+--
+ALTER TABLE `videos`
+  MODIFY `id_video` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `videos`
+--
+ALTER TABLE `videos`
+  ADD CONSTRAINT `videos_ibfk_1` FOREIGN KEY (`fk_id_competition`) REFERENCES `competitions` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
