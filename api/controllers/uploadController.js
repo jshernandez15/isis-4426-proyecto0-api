7d962be4246ce78e7ccddab2ef7ed0eeea233a54
Cristian Sepulveda <cf.sepulveda@uniandes.edu.co>
2018-10-21 14:11:07 -0500

    upload s3 in progress

--- api/controllers/uploadController.js
+++ api/controllers/uploadController.js
@@ -1,6 +1,16 @@
 const IncomingForm = require('formidable').IncomingForm;
+var AWS = require("aws-sdk");
+var fs = require('fs');
+
 
 module.exports = function upload(req, res) {
+
+    let s3bucket = new AWS.S3({
+        accessKeyId: process.env.KEYID,
+        secretAccessKey: process.env.SECRETKEYID,
+    });
+    AWS.config.region = "us-west-2"; //us-west-2 is Oregon
+
     var form = new IncomingForm();
     var response = {};
 
@@ -9,10 +19,23 @@ module.exports = function upload(req, res) {
 
     form.on('file', (field, file) => {
         response = file;
-        // Do something with the file
-        // e.g. save it to the database
-        // you can access it using file.path
-        //res.end(util.inspect({ fields: field, files: file }));
+        var params = {
+            Bucket: "4426-grupo1-videos",
+            Key: field,
+            Body: "Valueeee or some data"
+        };
+
+        fs.readFile(response.path, function (err, data) {
+            if (err) { throw err; }
+            params = { Bucket: "4426-grupo1-videos/original", Key: response.name, Body: data };
+            s3bucket.upload(params, function (err, data) {
+                if (err) {
+                    console.log('error in callback');
+                }
+                console.log('success');
+            });
+        });
+
     });
     form.on('end', () => {
         res.json(response);
