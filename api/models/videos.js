e644d1ab6c5cf7e1b279d2893373da724ae51202
oh.urrego@uniandes.edu.co <oh.urrego@uniandes.edu.co>
2018-10-21 13:53:51 -0500

    Add SQS sendind message

--- api/models/videos.js
+++ api/models/videos.js
@@ -2,219 +2,241 @@
 
 var AWS = require("aws-sdk");
 var uuid = require('uuid');
+var Producer = require('sqs-producer');
 
+var SESCREDENTIALS = {
+    accessKeyId: process.env.KEYID || '',
+    secretAccessKey: process.env.SECRETKEYID || ''
+};
 
-exports.list = function (param, callback) {
-  //order by id_video desc
-  AWS.config.update({
-    accessKeyId: process.env.KEYID,
-    secretAccessKey: process.env.SECRETKEYID
-  });
-  AWS.config.region = "us-west-2"; //us-west-2 is Oregon
+var producer = Producer.create({
+    queueUrl: 'https://sqs.us-west-2.amazonaws.com/994147617895/sqsGrupo1',
+    region: 'us-west-2',
+    accessKeyId: SESCREDENTIALS.accessKeyId,
+    secretAccessKey: SESCREDENTIALS.secretAccessKey
+});
 
-  var docClient = new AWS.DynamoDB.DocumentClient();
-  var params = {
-    TableName: "videos",
-    FilterExpression: "#fk_id_competition = :fk_id_competition and #state_video = :state_video",
-    ExpressionAttributeNames: {
-      "#fk_id_competition": "fk_id_competition",
-      "#state_video": "state_video"
-    },
-    ExpressionAttributeValues: { ":fk_id_competition": param.competitionId, ":state_video": param.state }
-  };
+exports.list = function(param, callback) {
+    //order by id_video desc
+    AWS.config.update({
+        accessKeyId: process.env.KEYID,
+        secretAccessKey: process.env.SECRETKEYID
+    });
+    AWS.config.region = "us-west-2"; //us-west-2 is Oregon
 
-  docClient.scan(params, function (err, data) {
-    if (err) {
-      console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
-      callback({ code: 500 });
-    } else {
-      callback({ list: data.Items });
-    }
-  });
+    var docClient = new AWS.DynamoDB.DocumentClient();
+    var params = {
+        TableName: "videos",
+        FilterExpression: "#fk_id_competition = :fk_id_competition and #state_video = :state_video",
+        ExpressionAttributeNames: {
+            "#fk_id_competition": "fk_id_competition",
+            "#state_video": "state_video"
+        },
+        ExpressionAttributeValues: { ":fk_id_competition": param.competitionId, ":state_video": param.state }
+    };
+
+    docClient.scan(params, function(err, data) {
+        if (err) {
+            console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+            callback({ code: 500 });
+        } else {
+            callback({ list: data.Items });
+        }
+    });
 
 };
 
-exports.listById = function (param, callback) {
-  //order by id_video desc
-  AWS.config.update({
-    accessKeyId: process.env.KEYID,
-    secretAccessKey: process.env.SECRETKEYID
-  });
-  AWS.config.region = "us-west-2"; //us-west-2 is Oregon
-  var docClient = new AWS.DynamoDB.DocumentClient();
-  var params = {
-    TableName: "videos",
-    FilterExpression: "#fk_id_competition = :fk_id_competition",
-    ExpressionAttributeNames: {
-      "#fk_id_competition": "fk_id_competition"
-    },
-    ExpressionAttributeValues: { ":fk_id_competition": param.competitionId }
-  };
+exports.listById = function(param, callback) {
+    //order by id_video desc
+    AWS.config.update({
+        accessKeyId: process.env.KEYID,
+        secretAccessKey: process.env.SECRETKEYID
+    });
+    AWS.config.region = "us-west-2"; //us-west-2 is Oregon
+    var docClient = new AWS.DynamoDB.DocumentClient();
+    var params = {
+        TableName: "videos",
+        FilterExpression: "#fk_id_competition = :fk_id_competition",
+        ExpressionAttributeNames: {
+            "#fk_id_competition": "fk_id_competition"
+        },
+        ExpressionAttributeValues: { ":fk_id_competition": param.competitionId }
+    };
 
-  docClient.scan(params, function (err, data) {
-    if (err) {
-      console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
-      callback({ code: 500 });
-    } else {
-      callback({ list: data.Items });
-    }
-  });
+    docClient.scan(params, function(err, data) {
+        if (err) {
+            console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+            callback({ code: 500 });
+        } else {
+            callback({ list: data.Items });
+        }
+    });
 };
 
-exports.create = function (video, callback) {
-  var d = new Date();
-  var dateCreated =
-    "" +
-    d.getFullYear() +
-    "-" +
-    d.getMonth() +
-    "-" +
-    d.getDay() +
-    " " +
-    d.getHours() +
-    "-" +
-    d.getMinutes() +
-    "-" +
-    d.getSeconds();
+exports.create = function(video, callback) {
+    var d = new Date();
+    var dateCreated =
+        "" +
+        d.getFullYear() +
+        "-" +
+        d.getMonth() +
+        "-" +
+        d.getDay() +
+        " " +
+        d.getHours() +
+        "-" +
+        d.getMinutes() +
+        "-" +
+        d.getSeconds();
 
-  AWS.config.update({
-    accessKeyId: process.env.KEYID,
-    secretAccessKey: process.env.SECRETKEYID
-  });
-  AWS.config.region = "us-west-2"; //us-west-2 is Oregon
+    AWS.config.update({
+        accessKeyId: process.env.KEYID,
+        secretAccessKey: process.env.SECRETKEYID
+    });
+    AWS.config.region = "us-west-2"; //us-west-2 is Oregon
 
-  var ddb = new AWS.DynamoDB();
+    var ddb = new AWS.DynamoDB();
 
 
-  var params = {
-    TableName: 'videos',
-    Item: {
-      "id": { "S": uuid.v1() },
-      "name": { "S": video.name },
-      "last_name": { "S": video.lastName },
-      "email": { "S": video.email },
-      "path_real": { "S": video.path },
-      "description": { "S": video.description },
-      "fk_id_competition": { "S": video.idConcurso.toString() },
-      "path_convertido": { "S": "empty" },
-      "state_video": { "S": video.stateVideo }
+    var params = {
+        TableName: 'videos',
+        Item: {
+            "id": { "S": uuid.v1() },
+            "name": { "S": video.name },
+            "last_name": { "S": video.lastName },
+            "email": { "S": video.email },
+            "path_real": { "S": video.path },
+            "description": { "S": video.description },
+            "fk_id_competition": { "S": video.idConcurso.toString() },
+            "path_convertido": { "S": "empty" },
+            "state_video": { "S": video.stateVideo }
+        }
+    };
+
+    var sqsMessage = {
+        "id": params.Item.id,
+        "path_real": params.Item.path_real,
+        "state_video": params.Item.state_video,
+        "email": params.Item.email
     }
-  };
 
-  ddb.putItem(params, function (err, data) {
-    if (err) {
-      console.log("Error", err);
-      callback(false);
-    } else {
-      console.log("Success", data);
-      callback(data);
-    }
-  });
+    ddb.putItem(params, function(err, data) {
+        if (err) {
+            console.log("Error", err);
+            callback(false);
+        } else {
+            producer.send([sqsMessage], function(err) {
+                if (err) console.log(err);
+            });
+            console.log("Success", data);
+            callback(data);
+        }
+    });
 };
 
-exports.find = function (id, callback) {
+exports.find = function(id, callback) {
 
-  AWS.config.update({
-    accessKeyId: process.env.KEYID,
-    secretAccessKey: process.env.SECRETKEYID
-  });
-  AWS.config.region = "us-west-2"; //us-west-2 is Oregon
+    AWS.config.update({
+        accessKeyId: process.env.KEYID,
+        secretAccessKey: process.env.SECRETKEYID
+    });
+    AWS.config.region = "us-west-2"; //us-west-2 is Oregon
 
-  var docClient = new AWS.DynamoDB.DocumentClient();
-  var params = {
-    TableName: "videos",
-    FilterExpression: "#fk_id_competition = :fk_id_competition",
-    ExpressionAttributeNames: {
-      "#fk_id_competition": "fk_id_competition",
-    },
-    ExpressionAttributeValues: { ":fk_id_competition": id }
+    var docClient = new AWS.DynamoDB.DocumentClient();
+    var params = {
+        TableName: "videos",
+        FilterExpression: "#fk_id_competition = :fk_id_competition",
+        ExpressionAttributeNames: {
+            "#fk_id_competition": "fk_id_competition",
+        },
+        ExpressionAttributeValues: { ":fk_id_competition": id }
 
-  };
+    };
 
-  docClient.scan(params, function (err, data) {
-    if (err) {
-      console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
-      callback({ code: 500 });
-    } else {
-      callback({ competition: data.Items[0] })
-    }
-  });
+    docClient.scan(params, function(err, data) {
+        if (err) {
+            console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+            callback({ code: 500 });
+        } else {
+            callback({ competition: data.Items[0] })
+        }
+    });
 
 };
 
-exports.update = function (competition, id, callback) {
-  var d = new Date();
-  var dateCreated =
-    "" +
-    d.getFullYear() +
-    "-" +
-    d.getMonth() +
-    "-" +
-    d.getDay() +
-    " " +
-    d.getHours() +
-    "-" +
-    d.getMinutes() +
-    "-" +
-    d.getSeconds();
+exports.update = function(competition, id, callback) {
+    var d = new Date();
+    var dateCreated =
+        "" +
+        d.getFullYear() +
+        "-" +
+        d.getMonth() +
+        "-" +
+        d.getDay() +
+        " " +
+        d.getHours() +
+        "-" +
+        d.getMinutes() +
+        "-" +
+        d.getSeconds();
 
-  AWS.config.update({
-    accessKeyId: process.env.KEYID,
-    secretAccessKey: process.env.SECRETKEYID
-  });
-  AWS.config.region = "us-west-2"; //us-west-2 is Oregon
+    AWS.config.update({
+        accessKeyId: process.env.KEYID,
+        secretAccessKey: process.env.SECRETKEYID
+    });
+    AWS.config.region = "us-west-2"; //us-west-2 is Oregon
 
-  var ddb = new AWS.DynamoDB();
+    var ddb = new AWS.DynamoDB();
 
 
-  var params = {
-    TableName: 'videos',
-    Item: {
-      "id": { "S": uuid.v1() },
-      "name": { "S": video.name },
-      "last_name": { "S": video.lastName },
-      "email": { "S": video.email },
-      "path_real": { "S": video.path },
-      "description": { "S": video.description },
-      "fk_id_competition": { N: video.idConcurso.toString() },
-      "path_convertido": { "S": "empty" },
-      "state_video": { "S": video.stateVideo }
-    }
-  };
+    var params = {
+        TableName: 'videos',
+        Item: {
+            "id": { "S": uuid.v1() },
+            "name": { "S": video.name },
+            "last_name": { "S": video.lastName },
+            "email": { "S": video.email },
+            "path_real": { "S": video.path },
+            "description": { "S": video.description },
+            "fk_id_competition": { N: video.idConcurso.toString() },
+            "path_convertido": { "S": "empty" },
+            "state_video": { "S": video.stateVideo }
+        }
+    };
 
-  ddb.putItem(params, function (err, data) {
-    if (err) {
-      console.log("Error", err);
-      callback({ code: 500 });
-    } else {
-      console.log("Success", data);
-      callback({ code: 200 });
-    }
-  });
+    ddb.putItem(params, function(err, data) {
+        if (err) {
+            console.log("Error", err);
+            callback({ code: 500 });
+        } else {
+            console.log("Success", data);
+            callback({ code: 200 });
+        }
+    });
 };
 
-exports.delete = function (id, callback) {
-  AWS.config.update({
-    accessKeyId: process.env.KEYID,
-    secretAccessKey: process.env.SECRETKEYID
-  });
-  AWS.config.region = "us-west-2"; //us-west-2 is Oregon
+exports.delete = function(id, callback) {
+    AWS.config.update({
+        accessKeyId: process.env.KEYID,
+        secretAccessKey: process.env.SECRETKEYID
+    });
+    AWS.config.region = "us-west-2"; //us-west-2 is Oregon
 
-  var docClient = new AWS.DynamoDB.DocumentClient();
+    var docClient = new AWS.DynamoDB.DocumentClient();
 
-  var params = {
-    TableName: "videos",
-    Key: {
-      "id": id
-    }
-  };
+    var params = {
+        TableName: "videos",
+        Key: {
+            "id": id
+        }
+    };
 
-  docClient.delete(params, function (err, data) {
-    if (err) {
-      callback({ code: 500 });
-    } else {
-      callback({ code: 200 });
-    }
-  });
+    docClient.delete(params, function(err, data) {
+        if (err) {
+            callback({ code: 500 });
+        } else {
+            callback({ code: 200 });
+        }
+    });
 
 };
